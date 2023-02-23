29473959e8b98b0c51f26775a99c9cc5e5bc708d
Matheus T. dos Santos <matheus.santos@edge.ufal.br>
2023-02-23 11:26:40 -0300

    Atualizando a aula 1 e movendo conteúdos para a aula 2
    Signed-off-by: Matheus T. dos Santos <matheus.santos@edge.ufal.br>

--- Modulo2/aula1/src/main.rs
+++ Modulo2/aula1/src/main.rs
@@ -1,97 +1,98 @@
 #![feature(never_type)]
 
-mod static_vars {
-    mod imutable {
-        static NAME: &'static str = "Matheus";
-        static AGE: u8 = 27;
+mod static_vs_const {
+    mod static_vars {
+        mod imutable {
+            static NAME: &'static str = "Matheus";
+            static AGE: u8 = 27;
 
-        #[test]
-        fn static_name_n_age() {
-            println!("Name: {}, age: {}", NAME, AGE);
+            #[test]
+            fn static_name_n_age() {
+                println!("Name: {}, age: {}", NAME, AGE);
+            }
+
+            #[test]
+            fn static_inside_fn() {
+                static AGE: u8 = 16;
+
+                println!("Age: {}", AGE);
+            }
         }
 
-        #[test]
-        fn static_inside_fn() {
-            static AGE: u8 = 16;
+        mod unsafe_mut {
+            static mut UNSAFE_COUNTER: u32 = 0;
 
-            println!("Age: {}", AGE);
+            #[test]
+            fn unsafe_mut_static() {
+                unsafe {
+                    UNSAFE_COUNTER += 1;
+                    println!("Counter: {}", UNSAFE_COUNTER);
+                }
+            }
         }
-    }
 
-    mod unsafe_mut {
-        static mut UNSAFE_COUNTER: u32 = 0;
+        mod safe_mut {
+            use std::sync::atomic::{AtomicU32, Ordering};
+            use std::sync::Mutex;
 
-        #[test]
-        fn unsafe_mut_static() {
-            unsafe {
-                UNSAFE_COUNTER += 1;
-                println!("Counter: {}", UNSAFE_COUNTER);
+            static SAFE_ARC_COUNTER: AtomicU32 = AtomicU32::new(0);
+
+            #[test]
+            fn atomic_mut_static() {
+                SAFE_ARC_COUNTER.fetch_add(1, Ordering::AcqRel);
+                println!("Counter: {}", SAFE_ARC_COUNTER.load(Ordering::Acquire));
+            }
+
+            static SAFE_MUTEX_COUNTER: Mutex<String> = Mutex::new(String::new());
+
+            #[test]
+            fn mutex_mut_static() {
+                let mut value = SAFE_MUTEX_COUNTER.lock().unwrap();
+                *value += "Hello";
+                println!("Counter: {}", value);
             }
         }
     }
 
-    mod safe_mut {
-        use std::sync::atomic::{AtomicU32, Ordering};
-        use std::sync::Mutex;
-
-        static SAFE_ARC_COUNTER: AtomicU32 = AtomicU32::new(0);
+    mod const_vars {
+        const PI: f32 = 3.14;
 
         #[test]
-        fn atomic_mut_static() {
-            SAFE_ARC_COUNTER.fetch_add(1, Ordering::AcqRel);
-            println!("Counter: {}", SAFE_ARC_COUNTER.load(Ordering::Acquire));
+        fn inside_fn() {
+            const MATH_PI: f32 = 3.14159265359;
+
+            println!("π: {}, math π: {}", PI, MATH_PI);
         }
 
-        static SAFE_MUTEX_COUNTER: Mutex<String> = Mutex::new(String::new());
+        struct Engineer;
+
+        impl Engineer {
+            const PI: u8 = 3;
+        }
 
         #[test]
-        fn mutex_mut_static() {
-            let mut value = SAFE_MUTEX_COUNTER.lock().unwrap();
-            *value += "Hello";
-            println!("Counter: {}", value);
+        fn engineer() {
+            println!("engineer π: {}", Engineer::PI);
         }
     }
-}
 
-mod const_vars {
-    const PI: f32 = 3.14;
-
-    #[test]
-    fn inside_fn() {
-        const MATH_PI: f32 = 3.14159265359;
-
-        println!("π: {}, math π: {}", PI, MATH_PI);
+    /// Diferenças de `const` e `static`
+    ///
+    /// |                       | const | static |
+    /// |:----------------------|:-----:|:------:|
+    /// | Tipos Explicito       | Sempre              | Sempre |
+    /// | Mutabilidade          | Nunca               | `unsafe` |
+    /// | Endereço fixo         | ❌                  | ✅ |
+    /// | É copiado em cada uso | ✅                  | ❌ |
+    /// | Tempo de execução     | Rápido              | Lento |
+    /// | Semelhança com C      | `#define VAR 0`     | `const uint8_t var = 0;` |
+    /// | Inicialização         | Sempre na definição | Sempre na definição |
+    mod const_vs_static {
+        #[allow(unused)]
+        const CONST: [u8; 1_000_000] = [0u8; 1_000_000];
+        #[allow(unused)]
+        static STATIC: [u8; 1_000_000] = [0u8; 1_000_000];
     }
-
-    struct Engineer;
-
-    impl Engineer {
-        const PI: u8 = 3;
-    }
-
-    #[test]
-    fn engineer() {
-        println!("engineer π: {}", Engineer::PI);
-    }
-}
-
-/// Diferenças de `const` e `static`
-///
-/// |                       | const | static |
-/// |:----------------------|:-----:|:------:|
-/// | Tipos Explicito       | Sempre              | Sempre |
-/// | Mutabilidade          | Nunca               | `unsafe` |
-/// | Endereço fixo         | ❌                  | ✅ |
-/// | É copiado em cada uso | ✅                  | ❌ |
-/// | Tempo de execução     | Rápido              | Lento |
-/// | Tamanho do binário    | Grande              | Pequeno |
-/// | Semelhança com C      | `#define VAR 0`     | `const uint8_t var = 0;` |
-/// | Inicialização         | Sempre na definição | Sempre na definição |
-mod const_vs_static {
-    #[allow(unused)]
-    const CONST: [u8; 1_000_000] = [0u8; 1_000_000];
-    #[allow(unused)]
-    static STATIC: [u8; 1_000_000] = [0u8; 1_000_000];
 }
 
 mod memory_ownership {
@@ -251,7 +252,7 @@ mod memory_ownership {
             println!("mt2: {mt2:?}");
         }
 
-        // !!!RECAP!!!
+        // !!! RECAP !!!
         // Tipos compostos só podem ser copiados se seus tipos puderem ser copiados. Se pelo menos
         // um dos tipos (no caso do array o tipo principal) não puder ser movido, então a tupla
         // (ou o array) não poderá ser copiado, será movido.
@@ -281,6 +282,7 @@ mod memory_ownership {
         }
 
         #[test]
+        #[allow(unused)]
         fn chalenge1() {
             fn greetings(p: Person) {
                 println!("Hello \"{}\"!", p.name);
@@ -339,18 +341,18 @@ mod memory_ownership {
     }
 }
 
-/// TODO
-mod borrow_checker {}
-
 mod trivia {
     use std::time::Duration;
 
     #[test]
+    #[allow(unreachable_code)]
+    #[allow(unused_variables)]
     fn never_type() {
         let a: ! = loop {};
         let p: ! = panic!();
     }
 
+    #[allow(unused)]
     fn blink() -> ! {
         let val = false;
         loop {
@@ -371,261 +373,12 @@ mod trivia {
     }
 
     #[test]
+    #[allow(unused)]
     fn enum_zero_variant() {
         enum ZeroVariant {}
 
         // let no_variant = ???;
     }
 }
-
-// mod move_string {
-//     fn digits_to_string(digit_list: &[u8]) -> String {
-//         /* É criada a variável 'string' no escopo da função 'digits_to_string'. */
-//         /* O buffer da String é alocado na heap, porém um "handler" é alocado na stack. */
-//         let mut string = String::new();
-//
-//         for digit in digit_list {
-//             string.push(char::from_digit(*digit as u32, 10).unwrap());
-//         }
-//
-//         /* A variável 'string' não é destruida aqui, mas sim movida para fora da função. Em C, a
-//         variável seria destruída e o seu valor copiado para a variável que recebe o retorno da
-//         função. */
-//         /* Como a variável é movida, o "handler" da String não sai do escopo e não é destruído.
-//         Desta forma, o buffer da String não é desalocado da heap. */
-//         string
-//     }
-//
-//     #[test]
-//     fn convert_to_string() {
-//         let digits = [1, 2, 3, 4, 5];
-//         let digits_str = digits_to_string(&digits);
-//         /* A variável 'string' criada na função 'digits_to_string' foi movida para a
-//         variável 'digits_str'. Note que a variável não foi copiada para fora como C, mas sim
-//         movida. A operação de mover uma variável aumenta o desempenho (uso de CPU e de memória)
-//         porque não é necessário copia a variável. */
-//
-//         println!("Digits: {}", digits_str);
-//
-//         /* No fim do escopo da função a variável 'digits_str' é desalocada ("destruída") da stack
-//         e sua parte na heap é desalocada no mesmo momento. */
-//     }
-// }
-//
-// mod move_my_string {
-//     #[derive(Debug)]
-//     struct MyString(String);
-//
-//     fn digits_to_my_string(digit_list: &[u8]) -> MyString {
-//         let mut string = MyString(String::new());
-//
-//         for digit in digit_list {
-//             string.0.push(char::from_digit(*digit as u32, 10).unwrap());
-//         }
-//
-//         string
-//     }
-//
-//     #[test]
-//     fn convert_to_my_string() {
-//         {
-//             let digits = [1, 2, 3, 4, 5];
-//             let digits_str = digits_to_my_string(&digits);
-//
-//             println!("Digits: {:?}", digits_str);
-//         }
-//         println!("Depois do escopo");
-//     }
-// }
-//
-// mod move_my_string_with_drop {
-//     #[derive(Debug)]
-//     struct MyString(String);
-//
-//     impl Drop for MyString {
-//         fn drop(&mut self) {
-//             println!("Droping MyString");
-//         }
-//     }
-//
-//     fn digits_to_my_string(digit_list: &[u8]) -> MyString {
-//         let mut string = MyString(String::new());
-//
-//         for digit in digit_list {
-//             string.0.push(char::from_digit(*digit as u32, 10).unwrap());
-//         }
-//
-//         string
-//     }
-//
-//     #[test]
-//     fn convert_to_my_string() {
-//         {
-//             let digits = [1, 2, 3, 4, 5];
-//             let digits_str = digits_to_my_string(&digits);
-//
-//             println!("Digits: {:?}", digits_str);
-//         }
-//         println!("Depois do escopo");
-//     }
-// }
-//
-// mod borrow_checker {
-//     /*
-//         Regras do Borrow Checker (Verificador de Empréstimos)
-//
-//         1. É possivel ter infinitas referências IMUTÁVEL (&) ao mesmo tempo.
-//         2. Se existir uma referência MUTÁVEL (&mut), não pode existir nenhum outro tipo de
-//            referência. Nem mutável (&mut), nem imutável (&).
-//     */
-//
-//     use std::vec;
-//
-//     #[test]
-//     fn one_imut() {
-//         let a = 10;
-//         let a_ref = &a;
-//
-//         println!("{}: {}", a, a_ref);
-//     }
-//
-//     #[test]
-//     fn many_imut() {
-//         let a = 10;
-//         let ref1 = &a;
-//         let ref2 = &a;
-//         let ref3 = ref2;
-//
-//         println!("{}: {}, {}, {}", a, ref1, ref2, ref3);
-//     }
-//
-//     #[test]
-//     fn one_mut() {
-//         let mut a = 10;
-//         let mut_ref = &mut a;
-//
-//         println!("{}", mut_ref);
-//     }
-//
-//     #[test]
-//     fn two_mut() {
-//         let mut a = 10;
-//         let ref1 = &mut a;
-//         // let ref2 = &mut a;
-//
-//         println!("{}", ref1);
-//         // println!("{}", ref2);
-//     }
-//
-//     #[test]
-//     fn one_mut_one_imut() {
-//         let mut a = 10;
-//         let mut_ref = &mut a;
-//         // let imut_ref = &a;
-//
-//         // println!("{}", imut_ref);
-//         println!("{}", mut_ref);
-//     }
-//
-//     mod point {
-//         #[derive(Clone)]
-//         pub struct Point {
-//             x: i32,
-//             #[allow(unused)]
-//             y: i32,
-//         }
-//
-//         impl Point {
-//             pub fn new(x: i32, y: i32) -> Point {
-//                 Point { x, y }
-//             }
-//
-//             pub fn set_x(&mut self, x: i32) {
-//                 self.x = x;
-//             }
-//
-//             pub fn get_x(&self) -> &i32 {
-//                 &self.x
-//             }
-//         }
-//     }
-//
-//     #[test]
-//     #[allow(unused_variables)]
-//     fn struct_mut_ref() {
-//         let mut p1 = point::Point::new(1, 2);
-//
-//         let p1_x = p1.get_x();
-//
-//         p1.set_x(3);
-//
-//         // println!("{}", p1_x);
-//     }
-//
-//     #[test]
-//     fn for_problem() {
-//         let mut points = vec![point::Point::new(1, 2)];
-//
-//         for p in points.iter_mut() {
-//             // p.set_x(p.get_x() + 1);
-//
-//             if *p.get_x() == 2 {
-//                 // points.remove(0);
-//             }
-//         }
-//     }
-//
-//     fn print_out(item: &Vec<u8>) {
-//         for i in item {
-//             println!("{}", i);
-//         }
-//     }
-//
-//     #[test]
-//     fn test_print_out() {
-//         let item = vec![1, 2, 3];
-//
-//         print_out(&item);
-//         print_out(&item);
-//     }
-//
-//     #[test]
-//     fn print_last() {
-//         let mut item = vec![1, 2, 3];
-//
-//         let last = item.last().unwrap();
-//         println!("Last: {}", last);
-//
-//         item.pop();
-//
-//         // println!("Last: {}", last);
-//     }
-//
-//     fn reverse_and_print(list: &Vec<u8>) {
-//         for li in list.iter().rev() {
-//             println!("{}", li);
-//         }
-//     }
-//
-//     fn reverse_and_print_mut(list: &mut Vec<u8>) {
-//         list.reverse();
-//         for li in list.iter() {
-//             println!("{}", li);
-//         }
-//     }
-//
-//     #[test]
-//     fn test_reverse_and_print() {
-//         let list = vec![1, 2, 3];
-//
-//         reverse_and_print(&list);
-//         reverse_and_print(&list);
-//
-//         let mut list = vec![4, 5, 6];
-//
-//         reverse_and_print_mut(&mut list);
-//         reverse_and_print_mut(&mut list);
-//     }
-// }
 
 fn main() {}
---
+++ Modulo2/aula2/src/main.rs
@@ -0,0 +1,256 @@
+/// TODO
+mod borrow_checker {}
+
+// mod move_string {
+//     fn digits_to_string(digit_list: &[u8]) -> String {
+//         /* É criada a variável 'string' no escopo da função 'digits_to_string'. */
+//         /* O buffer da String é alocado na heap, porém um "handler" é alocado na stack. */
+//         let mut string = String::new();
+//
+//         for digit in digit_list {
+//             string.push(char::from_digit(*digit as u32, 10).unwrap());
+//         }
+//
+//         /* A variável 'string' não é destruida aqui, mas sim movida para fora da função. Em C, a
+//         variável seria destruída e o seu valor copiado para a variável que recebe o retorno da
+//         função. */
+//         /* Como a variável é movida, o "handler" da String não sai do escopo e não é destruído.
+//         Desta forma, o buffer da String não é desalocado da heap. */
+//         string
+//     }
+//
+//     #[test]
+//     fn convert_to_string() {
+//         let digits = [1, 2, 3, 4, 5];
+//         let digits_str = digits_to_string(&digits);
+//         /* A variável 'string' criada na função 'digits_to_string' foi movida para a
+//         variável 'digits_str'. Note que a variável não foi copiada para fora como C, mas sim
+//         movida. A operação de mover uma variável aumenta o desempenho (uso de CPU e de memória)
+//         porque não é necessário copia a variável. */
+//
+//         println!("Digits: {}", digits_str);
+//
+//         /* No fim do escopo da função a variável 'digits_str' é desalocada ("destruída") da stack
+//         e sua parte na heap é desalocada no mesmo momento. */
+//     }
+// }
+//
+// mod move_my_string {
+//     #[derive(Debug)]
+//     struct MyString(String);
+//
+//     fn digits_to_my_string(digit_list: &[u8]) -> MyString {
+//         let mut string = MyString(String::new());
+//
+//         for digit in digit_list {
+//             string.0.push(char::from_digit(*digit as u32, 10).unwrap());
+//         }
+//
+//         string
+//     }
+//
+//     #[test]
+//     fn convert_to_my_string() {
+//         {
+//             let digits = [1, 2, 3, 4, 5];
+//             let digits_str = digits_to_my_string(&digits);
+//
+//             println!("Digits: {:?}", digits_str);
+//         }
+//         println!("Depois do escopo");
+//     }
+// }
+//
+// mod move_my_string_with_drop {
+//     #[derive(Debug)]
+//     struct MyString(String);
+//
+//     impl Drop for MyString {
+//         fn drop(&mut self) {
+//             println!("Droping MyString");
+//         }
+//     }
+//
+//     fn digits_to_my_string(digit_list: &[u8]) -> MyString {
+//         let mut string = MyString(String::new());
+//
+//         for digit in digit_list {
+//             string.0.push(char::from_digit(*digit as u32, 10).unwrap());
+//         }
+//
+//         string
+//     }
+//
+//     #[test]
+//     fn convert_to_my_string() {
+//         {
+//             let digits = [1, 2, 3, 4, 5];
+//             let digits_str = digits_to_my_string(&digits);
+//
+//             println!("Digits: {:?}", digits_str);
+//         }
+//         println!("Depois do escopo");
+//     }
+// }
+//
+// mod borrow_checker {
+//     /*
+//         Regras do Borrow Checker (Verificador de Empréstimos)
+//
+//         1. É possivel ter infinitas referências IMUTÁVEL (&) ao mesmo tempo.
+//         2. Se existir uma referência MUTÁVEL (&mut), não pode existir nenhum outro tipo de
+//            referência. Nem mutável (&mut), nem imutável (&).
+//     */
+//
+//     use std::vec;
+//
+//     #[test]
+//     fn one_imut() {
+//         let a = 10;
+//         let a_ref = &a;
+//
+//         println!("{}: {}", a, a_ref);
+//     }
+//
+//     #[test]
+//     fn many_imut() {
+//         let a = 10;
+//         let ref1 = &a;
+//         let ref2 = &a;
+//         let ref3 = ref2;
+//
+//         println!("{}: {}, {}, {}", a, ref1, ref2, ref3);
+//     }
+//
+//     #[test]
+//     fn one_mut() {
+//         let mut a = 10;
+//         let mut_ref = &mut a;
+//
+//         println!("{}", mut_ref);
+//     }
+//
+//     #[test]
+//     fn two_mut() {
+//         let mut a = 10;
+//         let ref1 = &mut a;
+//         // let ref2 = &mut a;
+//
+//         println!("{}", ref1);
+//         // println!("{}", ref2);
+//     }
+//
+//     #[test]
+//     fn one_mut_one_imut() {
+//         let mut a = 10;
+//         let mut_ref = &mut a;
+//         // let imut_ref = &a;
+//
+//         // println!("{}", imut_ref);
+//         println!("{}", mut_ref);
+//     }
+//
+//     mod point {
+//         #[derive(Clone)]
+//         pub struct Point {
+//             x: i32,
+//             #[allow(unused)]
+//             y: i32,
+//         }
+//
+//         impl Point {
+//             pub fn new(x: i32, y: i32) -> Point {
+//                 Point { x, y }
+//             }
+//
+//             pub fn set_x(&mut self, x: i32) {
+//                 self.x = x;
+//             }
+//
+//             pub fn get_x(&self) -> &i32 {
+//                 &self.x
+//             }
+//         }
+//     }
+//
+//     #[test]
+//     #[allow(unused_variables)]
+//     fn struct_mut_ref() {
+//         let mut p1 = point::Point::new(1, 2);
+//
+//         let p1_x = p1.get_x();
+//
+//         p1.set_x(3);
+//
+//         // println!("{}", p1_x);
+//     }
+//
+//     #[test]
+//     fn for_problem() {
+//         let mut points = vec![point::Point::new(1, 2)];
+//
+//         for p in points.iter_mut() {
+//             // p.set_x(p.get_x() + 1);
+//
+//             if *p.get_x() == 2 {
+//                 // points.remove(0);
+//             }
+//         }
+//     }
+//
+//     fn print_out(item: &Vec<u8>) {
+//         for i in item {
+//             println!("{}", i);
+//         }
+//     }
+//
+//     #[test]
+//     fn test_print_out() {
+//         let item = vec![1, 2, 3];
+//
+//         print_out(&item);
+//         print_out(&item);
+//     }
+//
+//     #[test]
+//     fn print_last() {
+//         let mut item = vec![1, 2, 3];
+//
+//         let last = item.last().unwrap();
+//         println!("Last: {}", last);
+//
+//         item.pop();
+//
+//         // println!("Last: {}", last);
+//     }
+//
+//     fn reverse_and_print(list: &Vec<u8>) {
+//         for li in list.iter().rev() {
+//             println!("{}", li);
+//         }
+//     }
+//
+//     fn reverse_and_print_mut(list: &mut Vec<u8>) {
+//         list.reverse();
+//         for li in list.iter() {
+//             println!("{}", li);
+//         }
+//     }
+//
+//     #[test]
+//     fn test_reverse_and_print() {
+//         let list = vec![1, 2, 3];
+//
+//         reverse_and_print(&list);
+//         reverse_and_print(&list);
+//
+//         let mut list = vec![4, 5, 6];
+//
+//         reverse_and_print_mut(&mut list);
+//         reverse_and_print_mut(&mut list);
+//     }
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
